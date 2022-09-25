9c6cf6792bbe3064a4c79bce4738097eefa73abc
Adam Melhem <Adam Melhem@DESKTOP-NVK9QMH>
2022-09-25 13:28:32 +0300

    Add project files.

---
+++ StockMarket/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = new int[] {1,4,5,6,9,2,0,3};
+var stockMarketAnalizer = new StockMarketAnalizer();
+(int,int) result  =  stockMarketAnalizer.analyze(input);
+Console.WriteLine($"Rresult:{result}");
---
+++ StockMarket/StockMarketAnalizer.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class StockMarketAnalizer
+{ 
+
+    public StockMarketAnalizer( )
+    {
+    }
+
+    public (int, int) analyze(int[] input)
+    {
+        var len = input.Length;
+        int index_min = 0;
+        int index_max = len-1;
+        int min = int.MaxValue;
+        int max = 0;
+
+        // Big O = n
+        for (int i = 0; i < input.Length; i++ )
+        {
+            if (input[i]<min)
+            {
+                min = input[i];
+                index_min = i;
+            }
+
+            if (input[i] > max && index_max > index_min)
+            {
+                max = input[i];
+                index_max = i;
+            }
+        }
+
+        var result = (max , min);
+        return result;
+    }
+
+}
